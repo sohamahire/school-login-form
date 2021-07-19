36dcf9cf9e42362383a4ce586a95c9448ad2bb67
sohamahire <66758574+sohamahire@users.noreply.github.com>
2021-07-19 22:53:50 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -19,9 +19,29 @@ inputs.forEach(input => {
 	input.addEventListener("blur", remcl);
 });
 
+
 function dashboard()
 {
-	window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", '__blank')
+	window.open("pages/dashboard.html");
+	//window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+	window.close();
 	
 }
 
+function logOut()
+{
+	window.open ("file:///C:/Users/soham/OneDrive/Desktop/New folder/Responsive-Login-Form-master/login.html");
+}
+
+const showMenu = (toggleId,navId) =>
+{
+	const toggle = document.getElementById(toggleId),
+	nav = document.getElementById(navId);
+
+	if(toggle && nav)
+	{
+		toggle.addEventListener('click' , ()=>{
+			nav.classList.toggle('show');
+		})
+	}
+}
